1409da9610868031ebf8b4fede558757b2081632
quocdungms <quocdunms@gmail.com>
2025-02-24 20:30:16 +0700

    update

--- encode.py
+++ encode.py
@@ -7,5 +7,6 @@ def bits_to_bytearray(bit_string):
         raise ValueError("Chuỗi bit phải có độ dài là bội số của 8.")
     byte_array = bytearray(int(bit_string[i:i + 8], 2) for i in range(0, len(bit_string), 8))
     return byte_array
-data = '0101100101100000'
+# data = '0101100101100000'
+data = '1101100110100000'
 print(bits_to_bytearray(data))
