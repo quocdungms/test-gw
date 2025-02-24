327d786f8ecce7f955a3b0376d5d88c456d9e8b9
quocdungms <quocdunms@gmail.com>
2025-02-24 20:16:20 +0700

    update

--- op-check.py
+++ op-check.py
@@ -85,6 +85,7 @@ async def main():
     #device_address = "EB:C3:F1:BC:24:DD"  # Ví dụ, thay bằng MAC address thực tế
     for i in device_address:
         await fetch_operation_mode(i)
+        print("##############################################\n")
 
 
 if __name__ == "__main__":
